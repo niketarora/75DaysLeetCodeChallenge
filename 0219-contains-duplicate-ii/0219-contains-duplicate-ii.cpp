d231bac1b29ea7f8708cedec780dc33fd9290e8e
Niket Arora <niketarora682@gmail.com>
2026-04-04 10:55:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
-        for(int i=0; i<nums.size(); i++){
-            if(mp.find(nums[i]) != mp.end()){
-                if((i - mp[nums[i]]) <= k)  
-                    return true;
+        // USING UNORDERED_MAP
+        // unordered_map<int, int> mp;
+        // for(int i=0; i<nums.size(); i++){
+        //     if(mp.find(nums[i]) != mp.end()){
+        //         if((i - mp[nums[i]]) <= k)  
+        //             return true;
+        //     }
+        //     mp[nums[i]] = i;
+        // }
+        // return false;
+
+
+
+        // USING SLINGDING WINDOW + SET
+        unordered_set<int> st;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (st.find(nums[i]) != st.end()) {
+                return true;
             }
-            mp[nums[i]] = i;
+
+            st.insert(nums[i]);
+
+            if (st.size() > k) {
+                st.erase(nums[i - k]);
+            }
         }
+
         return false;
     }
 };
