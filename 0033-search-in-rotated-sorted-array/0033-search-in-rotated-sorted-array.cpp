a46ee1a56cc8ca1a32fb7e495034b2bac27b18a7
Niket Arora <niketarora682@gmail.com>
2026-04-07 07:20:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,9 +1,33 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] == target){
-                return i;
+        
+        // LINEAR SOLUTION
+        // for(int i=0; i<nums.size(); i++){
+        //     if(nums[i] == target){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        // BINARY SOLUTION
+        int n = nums.size();
+        int l = 0;
+        int h = n-1;
+        while(l <= h){
+            int mid = (l + h)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[l] <= nums[mid]){ // left sorted
+                if(nums[l] <= target && target <= nums[mid])
+                    h = mid - 1;
+                else    
+                    l = mid + 1;
+            }
+            else{   // right sorted
+                if(nums[mid] <= target && target <= nums[h])
+                    l = mid + 1;
+                else
+                    h = mid - 1;
             }
         }
         return -1;
