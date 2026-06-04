ee6f7ed593893b63fedf7273e99866bbb8fbc39a
Niket Arora <niketarora682@gmail.com>
2026-06-04 09:18:02 +0530

    Time: 0 ms (100%), Space: 12.5 MB (60.38%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -14,8 +14,9 @@ public:
     TreeNode* invertTree(TreeNode* root) {
         if(root == NULL)    return root;
         swap(root->left, root->right);
-        invertTree(root->left);
-        invertTree(root->right);
+        invertTree(root->left); 
+        invertTree(root->right); 
         return root;
+    
     }
 };
