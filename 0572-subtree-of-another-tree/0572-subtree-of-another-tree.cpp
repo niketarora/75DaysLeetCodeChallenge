c3d3ceb33e8088b0b80c6ae8ebf9464cef0fbb3b
Niket Arora <niketarora682@gmail.com>
2026-06-06 10:18:04 +0530

    Time: 2 ms (30.65%), Space: 29 MB (70%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -11,24 +11,17 @@
  */
 class Solution {
 public:
-    // Function to check if two trees are identical
-    bool isSame(TreeNode* root, TreeNode* subRoot) {
-        if (!root && !subRoot) return true;
-        if (!root || !subRoot) return false;
+    bool isSame(TreeNode* root, TreeNode* subRoot){
+        if(!root && !subRoot)   return true;
+        if(!root || !subRoot)   return false;
+        if(root->val != subRoot->val)   return false;
         
-        if (root->val != subRoot->val) return false;
-        
-        return isSame(root->left, subRoot->left) &&
-               isSame(root->right, subRoot->right);
+        return isSame(root->left, subRoot->left) && isSame(root->right, subRoot->right);
     }
-    
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if (!root) return false;
-        
-        // Check at current node OR check in left/right subtree
-        if (isSame(root, subRoot)) return true;
-        
-        return isSubtree(root->left, subRoot) ||
-               isSubtree(root->right, subRoot);
+        if(root == NULL)    return false;
+        if(isSame(root, subRoot))   return true;
+
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }
 };
