dd29e25d919201d455c763093f67f6ae9301df31
Niket Arora <niketarora682@gmail.com>
2026-06-08 22:46:29 +0530

    Time: 3 ms (77.46%), Space: 14.1 MB (75.14%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int lengthOfLIS(std::vector<int>& nums) {
+        if (nums.empty()) return 0;
+        
+        std::vector<int> tails;
+        for (int num : nums) {
+            auto it = std::lower_bound(tails.begin(), tails.end(), num);
+            if (it == tails.end()) {
+                tails.push_back(num);
+            } else {
+                *it = num;
+            }
+        }
+        
+        return tails.size();
+    }
+};
