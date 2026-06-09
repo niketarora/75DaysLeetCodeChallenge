46174a4a0b3a85e7c2d5cd59010cc0f2a5ca895e
Niket Arora <niketarora682@gmail.com>
2026-06-09 10:59:09 +0530

    Time: 3 ms (51.44%), Space: 13.9 MB (92.1%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    void combination(int idx, int target, vector<int>& nums, vector<vector<int>>& res, vector<int> ds){
-        if(idx == nums.size()){
-            if(target == 0) 
-                res.push_back(ds);
+    void combination(int idx, vector<int> &ds, vector<int>& candidates, int target, vector<vector<int>> &ans){
+        if(idx == candidates.size()){
+            if(target == 0){
+                ans.push_back(ds);
+            }
             return;
         }
-        // picking up the element
-        if(nums[idx] <= target){
-            ds.push_back(nums[idx]);
-            combination(idx, target-nums[idx], nums, res, ds);
+        // pick 
+        if(candidates[idx]<=target){
+            ds.push_back(candidates[idx]);
+            combination(idx, ds, candidates, target-candidates[idx], ans);
             ds.pop_back();
         }
-
-        combination(idx+1, target, nums, res, ds);
+        // not pick
+        combination((idx+1), ds, candidates, target, ans);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> res;
         vector<int> ds;
-        combination(0, target, candidates, res, ds);
-        return res;
+        vector<vector<int>> ans;
+        combination(0, ds, candidates, target, ans);
+        return ans;
     }
 };
