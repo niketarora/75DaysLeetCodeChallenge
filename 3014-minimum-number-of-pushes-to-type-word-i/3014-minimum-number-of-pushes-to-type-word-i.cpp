8fafd64753d17f7f3c56cf54a3f3b56982d851dd
Niket Arora <niketarora682@gmail.com>
2026-07-30 18:52:53 +0530

    Time: 3 ms (11.73%), Space: 8.9 MB (19.48%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -3,25 +3,29 @@ public:
     int minimumPushes(string word) {
         int res = 0;
         int assign = 2;
-        // unordered_map<int, int> mp;
-        // for(char &ch : word){
-        //     if(assign > 9){
-        //         assign = 2;
-        //     }
-        //     mp[assign]++;
-        //     res += mp[assign];
 
-        //     assign++;
-        // }
+        // using map
+        unordered_map<int, int> mp;
+        for(char &ch : word){
+            if(assign > 9){
+                assign = 2;
+            }
+            mp[assign]++;
+            res += mp[assign];
 
-        vector<int> v(10, 0);
-        for(int i=0; i<word.size(); i++){
-            if(assign > 9)  assign = 2;
-            
-            v[assign]++;
-            res += v[assign];
             assign++;
         }
+
+
+        // using vector 
+        // vector<int> v(10, 0);
+        // for(int i=0; i<word.size(); i++){
+        //     if(assign > 9)  assign = 2;
+            
+        //     v[assign]++;
+        //     res += v[assign];
+        //     assign++;
+        // }
 
         return res;
     }
