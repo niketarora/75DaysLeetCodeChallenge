d7c271e38c8529d166c50afa19d37dfa61666432
Niket Arora <niketarora682@gmail.com>
2026-08-06 16:37:56 +0530

    Time: 0 ms (100%), Space: 14.8 MB (96.15%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,30 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> result;
-        if (!root) return result;
-
+        vector<int> ans;
+        if(root == NULL)    return ans;
         queue<TreeNode*> q;
         q.push(root);
+        while(q.size()>0){
+            int n = q.size();
+            TreeNode* temp = NULL;
 
-        while (!q.empty()) {
-            int levelSize = q.size();
-            
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode* node = q.front();
+            while(n--){
+                temp = q.front();
+                if(temp->left != NULL)  q.push(temp->left);
+                if(temp->right != NULL)   q.push(temp->right);
                 q.pop();
-
-                // If this is the last node in the current level, add it to results
-                if (i == levelSize - 1) {
-                    result.push_back(node->val);
-                }
-
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
             }
+            ans.push_back(temp->val);
         }
-        
-        return result;
+        return ans;
     }
 };
-
